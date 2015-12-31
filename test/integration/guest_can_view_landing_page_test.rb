5db8f533c010b9debe318d7e98c61c5a9039f221
Pat Wey <patrickwey@gmail.com>
2015-12-31 16:16:10 -0700

    Add description to landing page, update test

--- test/integration/guest_can_view_landing_page_test.rb
+++ test/integration/guest_can_view_landing_page_test.rb
@@ -2,10 +2,13 @@ require 'test_helper'
 
 class GuestCanViewLandingPageTest < ActionDispatch::IntegrationTest
   test "guest can view landing page" do
+    description = "Communicate with one correspondent, once a day."
+
     visit "/"
 
     within ".content" do
       assert page.has_css? ".title", "Correspondence"
+      assert page.has_css? ".description", description
       assert page.has_link? "Get Started"
     end
   end
